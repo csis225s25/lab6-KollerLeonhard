8b6fb516b3ff21c2f5e906e3a422f4cf50f92524
Michael Leonhard <m10leon@siena.edu>
2025-02-27 11:58:16 -0500

    finished font and colors section

--- HelloGraphics.java
+++ HelloGraphics.java
@@ -11,24 +11,10 @@ import javax.swing.*;
 
 // a class that extends JPanel to override the paintComponent method,
 // allowing us to interact with Java's graphics system
-class GraphicsPanel extends JPanel {
-
-    @Override
-    public void paintComponent(Graphics g) {
-
-        // first, we should call the paintComponent method we are
-        // overriding in JPanel
-        super.paintComponent(g);
-
-        // the Graphics object passed to this method has many methods
-        // we can use to draw in the area of the panel, one of which
-        // allows us to draw a String at a given x,y position
-        g.drawString("Hello, Java Graphics World!", 0, 20);
-    }
-}
 
 public class HelloGraphics implements Runnable {
-
+    public JFrame frame;
+    public JPanel panel;
     /**
      * The run method to set up the graphical user interface
      */
@@ -37,12 +23,12 @@ public class HelloGraphics implements Runnable {
 
         // the usual JFrame setup steps
         JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("HelloGraphics");
+        frame = new JFrame("HelloGraphics");
         frame.setPreferredSize(new Dimension(500, 500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // construct JPanel with a custom paintComponent method
-        JPanel panel = new GraphicsPanel();
+        panel = new GraphicsPanel();
         frame.add(panel);
 
         // display the window we've created
@@ -50,7 +36,42 @@ public class HelloGraphics implements Runnable {
         frame.setVisible(true);
     }
 
+   
+    class GraphicsPanel extends JPanel {
+        public int panelWidth;
+        public int panelHeight;
+        public int stringWidth;
+        public int stringHeight;
+        public String outputString;
+        @Override
+        public void paintComponent(Graphics g) {
+            
+            // first, we should call the paintComponent method we are
+            // overriding in JPanel
+            super.paintComponent(g);
+            panelWidth = panel.getWidth();
+            panelHeight = panel.getHeight();
+            outputString = "Hello, Java Graphics World!";
+            FontMetrics fontMetrics = g.getFontMetrics();
+            stringWidth = fontMetrics.stringWidth(outputString);
+            stringHeight = fontMetrics.getAscent();
+            Font currentFont = g.getFont();
+            Font newFont = new Font("Monospaced",Font.BOLD , currentFont.getSize());
+            g.setFont(newFont);
+            panel.setForeground(Color.RED);
+            panel.setBackground(Color.BLUE);
+
+            
+    
+            // the Graphics object passed to this method has many methods
+            // we can use to draw in the area of the panel, one of which
+            // allows us to draw a String at a given x,y position
+            g.drawString(outputString, panelWidth /2 - stringWidth / 2, panelHeight /2 - stringHeight / 2);
+           
+        }
+    }
     public static void main(String args[]) {
         javax.swing.SwingUtilities.invokeLater(new HelloGraphics());
     }
+    
 }
